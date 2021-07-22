22bef31c7260efef21573ce1ce90430648b3853e
liangqiang <173228514@qq.com>
2021-07-22 17:02:38 +0800

    add-增加公网ip获取方法

--- proxy/src/main/java/com/liangqiang/proxy/utils/IPUtils.java
+++ proxy/src/main/java/com/liangqiang/proxy/utils/IPUtils.java
@@ -2,10 +2,12 @@ package com.liangqiang.proxy.utils;
 
 import lombok.extern.slf4j.Slf4j;
 
-import java.net.Inet4Address;
-import java.net.InetAddress;
-import java.net.NetworkInterface;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.*;
 import java.util.Enumeration;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Slf4j
 public class IPUtils {
@@ -28,5 +30,48 @@ public class IPUtils {
             log.error(e.getLocalizedMessage(), e);
         }
         return null;
+    }
+
+    public static String getPublicIp() {
+        // 这里使用jsonip.com第三方接口获取本地IP
+        String jsonip = "https://jsonip.com/";
+        // 接口返回结果
+        StringBuilder result = new StringBuilder();
+        BufferedReader in = null;
+        try {
+            // 使用HttpURLConnection网络请求第三方接口
+            URL url = new URL(jsonip);
+            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setRequestMethod("GET");
+            urlConnection.connect();
+            in = new BufferedReader(new InputStreamReader(
+                    urlConnection.getInputStream()));
+            String line;
+            while ((line = in.readLine()) != null) {
+                result.append(line);
+            }
+        } catch (Exception e) {
+            log.error(e.getLocalizedMessage(), e);
+        }
+        // 使用finally块来关闭输入流
+        finally {
+            try {
+                if (in != null) {
+                    in.close();
+                }
+            } catch (Exception e) {
+                log.error(e.getLocalizedMessage(), e);
+            }
+        }
+        // 正则表达式，提取xxx.xxx.xxx.xxx，将IP地址从接口返回结果中提取出来
+        String rexp = "(\\d{1,3}\\.){3}\\d{1,3}";
+        Pattern pat = Pattern.compile(rexp);
+        Matcher mat = pat.matcher(result.toString());
+        String res = "";
+        while (mat.find()) {
+            res = mat.group();
+            break;
+        }
+        return res;
     }
 }
